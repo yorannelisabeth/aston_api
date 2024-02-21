cf35bfc89627e48f56292edb6f69b9624faef8da
yorannelisabeth <yorannelisabeth@gmail.com>
2024-02-21 15:56:36 +0100

    add files

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Components
+import Dashboard from "./components/Dasboard";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import BookUser from "./components/BookUser";
+// Configuration React-Toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactTostify.css";
+// Style
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Login /> } />
+          <Route path="/register" element={ <Signup /> } />
+          <Route path="/dashboard" element={ <Dashboard /> } />
+          <Route path="/profile/:id" element={ <Profile /> } />
+          <Route path="/book/:user" element={ <BookUser /> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
